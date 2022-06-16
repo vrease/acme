4e36dba1e1e6108dd515b33efc5fb3def69f82e3
Jonathan Yngfors <35510079+vrease@users.noreply.github.com>
2022-06-16 22:29:31 +0200

    Add main structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <header></header>
+      <main>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
 };
 
 export default Home;
